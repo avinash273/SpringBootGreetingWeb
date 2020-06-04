6bebe216177ff94c4110b0a0ae8dad06d583fc93
avinash273 <avinash.shanker@mavs.uta.edu>
2020-06-04 01:43:06 -0500

    Initial commit

---
+++ src/main/java/com/rest/springBoot/Application.java
@@ -0,0 +1,40 @@
+package com.rest.springBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Building a classic hello world endpoint
+ */
+
+/**
+ * The hello() method we’ve added is designed to take a String parameter called name
+ * and then combine this parameter with the word "Hello" in the code.
+ * This means that if you set your name to “Amy” in the request, the response would be “Hello Amy”.
+ *
+ * The @RestController annotation tells Spring that this code describes an endpoint that
+ * should be made available over the web. The @GetMapping(“/hello”) tells Spring to use
+ * our hello() method to answer requests that get sent to the http://localhost:8080/hello address.
+ * Finally, the @RequestParam is telling Spring to expect a name value in the request, but if it’s
+ * not there, it will use the word “World” by default.
+ * http://localhost:8080/hello?name=avinash
+ */
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+}
+
+
